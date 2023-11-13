7ae7eb800409a838dba1b61da420c7ca28f0ce6c
hirrumi <BlueberryRainbows@yandex.ru>
2023-11-13 20:05:13 +0300

    Add clasmet and func for json_dumps

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,7 @@
 import json
 from googleapiclient.discovery import build
 
+
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = 'AIzaSyAvSJwZZPDCUm5njvSBpnx-41ubiskiQXg'
@@ -8,7 +9,15 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.service = self.get_service()
+        self.youtube_data = self.youtube_file()
+        self.title = self.youtube_data['snippet']['title']
+        self.description = self.youtube_data['snippet']['description']
+        self.url = 'https://www.youtube.com/' + self.youtube_data['snippet']['customUrl']
+        self.view_count = self.youtube_data['statistics']['viewCount']
+        self.video_count = self.youtube_data['statistics']['videoCount']
+        self.subscriber_count = self.youtube_data['statistics']['subscriberCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -17,6 +26,32 @@ class Channel:
             """ Выводит словарь в json-подобном удобном формате с отступами """
             print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
-        channels_id = self.channel_id
+        channels_id = self.__channel_id
         channel = self.youtube.channels().list(id=channels_id, part='snippet,statistics').execute()
         printj(channel)
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    def to_json(self, name):
+        with open(name, 'w'):
+            json_file = json.dumps({
+                'channel_id': self.__channel_id,
+                'title': self.title,
+                'description': self.description,
+                'custom_url': self.url,
+                'subscriber_count': self.subscriber_count,
+                'video_count': self.video_count,
+                'view_count': self.view_count
+            })
+        return json_file
+
+    def youtube_file(self):
+        channels_id = self.__channel_id
+        channel = Channel.get_service().channels().list(id=channels_id, part='snippet,statistics').execute()
+        return channel['items'][0]
